f74373718c1181e3f89d6089dbe41697b9508261
Jude Ma <dragonscripter03@gmail.com>
2024-09-17 20:34:33 -0400

    made the create button work and close as well as adding to database

--- Inventory/wwwroot/js/site.js
+++ Inventory/wwwroot/js/site.js
@@ -2,3 +2,19 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+function showForm()
+{
+    document.getElementById("form-container").style.display = "flex";
+}
+
+function closeForm()
+{
+    document.getElementById("form-container").style.display = 'none';
+}
+
+window.onclick = function (event) {
+    const outside = document.getElementById("form-container");
+    if (event.target == outside) {
+        closeForm();
+    }
+}
